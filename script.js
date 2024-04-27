f359723d74ed5ddb85fcc5967c3f6d44ca65bd21
ashxsong <beethoven6198@gmail.com>
2024-04-27 13:58:49 -0500

    Make buttons work with single digit arithmetic
    Enabled buttons to perform arithmetic with operands from 0 to 9

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add(num1, num2) {
-  return num1 + num2;
+  return parseFloat(num1) + parseFloat(num2);
 }
 
 function subtract(num1, num2) {
@@ -29,8 +29,9 @@ function operate(operator, num1, num2) {
   }
 }
 
-buttons = document.querySelectorAll("button");
-display = document.querySelector("#display");
+let buttons = document.querySelectorAll("button");
+let display = document.querySelector("#display");
+let arr = [];
 let displayValue = "";
 for (let button of buttons) {
   button.addEventListener("click", () => {
@@ -38,6 +39,24 @@ for (let button of buttons) {
       displayValue = document.createElement("span");
       displayValue.textContent = button.textContent;
       display.appendChild(displayValue);
+    }
+    if ("1234567890+-*/".includes(button.textContent)) {
+      arr.push(button.textContent);
+    }
+    else if (button.textContent === "AC") {
+      let spans = document.querySelectorAll("span");
+      for (let span of spans) {
+        span.remove();
+      }
+    }
+    else {
+      let num1 = arr[0];
+      let operator = arr[1];
+      let num2 = arr[2];
+      displayValue = document.createElement("span");
+      displayValue.textContent = operate(operator, num1, num2);
+      display.appendChild(displayValue);
+      arr = [];
     }
   });
 }
