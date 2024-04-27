c5de84053cb447c4bb9f85b8c0e9621a8a686038
ashxsong <beethoven6198@gmail.com>
2024-04-27 14:57:53 -0500

    Fix display with basic math operations
    Replaced first operand with second operand and later result in display

--- script.js
+++ script.js
@@ -32,8 +32,10 @@ function operate(operator, num1, num2) {
 let buttons = document.querySelectorAll("button");
 let display = document.querySelector("#display");
 let displayValue = document.createElement("span");
+let operation = [];
 
 const numbers = "1234567890";
+const operators = "+-*/";
 
 displayValue.textContent = "0";
 display.appendChild(displayValue);
@@ -41,12 +43,23 @@ display.appendChild(displayValue);
 for (let button of buttons) {
   button.addEventListener("click", () => {
     if (numbers.includes(button.textContent)) {
-      if (displayValue.textContent === "0") {
+      if (displayValue.textContent === "0" || operation.length !== 0) {
         displayValue.textContent = button.textContent;
+        if (operation[0] === "operation complete") {
+          operation = [];
+        }
       } else {
         displayValue.textContent += button.textContent;
       }
       display.appendChild(displayValue);
+    } else if (operators.includes(button.textContent)) {
+      operation.push(displayValue.textContent);
+      operation.push(button.textContent);
+    } else if (button.textContent === "=") {
+      operation.push(displayValue.textContent);
+      displayValue.textContent = operate(operation[1], operation[0], operation[2]);
+      display.appendChild(displayValue);
+      operation = ["operation complete"];
     }
   })
 }
